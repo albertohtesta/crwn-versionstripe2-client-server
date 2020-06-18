29ee0f71eae51a6cc0e0736e5e8282c52c484f4a
albert <albertohtesta@gmail.com>
2020-06-18 14:36:54 -0500

    añadi un log en server cuando se carga exitosamente

--- server.js
+++ server.js
@@ -40,8 +40,10 @@ app.post('/payment', (req, res) => {
   stripe.charges.create(body, (stripeErr, stripeRes) => {
     if (stripeErr) {
       res.status(500).send({ error: stripeErr });
+      console.log(stripeErr);
     } else {
       res.status(200).send({ success: stripeRes });
+      console.log('OK...');
     }
   });
 });
